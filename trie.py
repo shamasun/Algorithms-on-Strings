d6c4c2c951d6f98a28618a3be6538865fe9f5364
Bharathan <shamasun@users.noreply.github.com>
2018-03-19 13:52:25 +0530

    Trie Construction
    Python code for construction of a Trie.

---
+++ trie.py
@@ -0,0 +1,24 @@
+#Uses python3
+import sys
+
+def trie_contruct(patterns):
+    trie = {0:{}}
+    root = 0
+    for pattern in patterns:
+        currentNode = root
+        for char in list(pattern):
+            part = trie[currentNode]
+            if(char in part.keys()):
+                currentNode = part[char]
+            else:
+                currentNode = len(trie)
+                trie[currentNode] = {}
+                part[char] = currentNode
+    return(trie)
+
+if __name__ == '__main__':
+    patterns = sys.stdin.read().split()[1:]
+    tree = trie_contruct(myInput)
+    for node in tree:
+        for c in tree[node]:
+            print("{}->{}:{}".format(node, tree[node][c], c))
