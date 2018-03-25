0693af5a631e27e4b5bcb18de2bfd11bcc143d10
Bharathan <shamasun@users.noreply.github.com>
2018-03-25 20:13:13 +0530

    Programming Assignment 1
    Coursera grader comment for
    (1) Implenting Trie construction: Max time used: 0.07/2.00, max memory used: 67596288/536870912
    
    (2) Implementing Trie matching: Max time used: 2.68/7.00, max memory used: 191922176/536870912.

--- trie.py
+++ trie.py
@@ -1,6 +1,3 @@
-#Uses python3
-import sys
-
 def trie_contruct(patterns):
     trie = {0:{}}
     root = 0
@@ -15,10 +12,3 @@ def trie_contruct(patterns):
                 trie[currentNode] = {}
                 part[char] = currentNode
     return(trie)
-
-if __name__ == '__main__':
-    patterns = sys.stdin.read().split()[1:]
-    tree = trie_contruct(patterns)
-    for node in tree:
-        for c in tree[node]:
-            print("{}->{}:{}".format(node, tree[node][c], c))
--- trie_pattern_match.py
+++ trie_pattern_match.py
@@ -1,53 +1,23 @@
-#Uses python3
-import sys
-
-def prefix_trie_match(text, trie):
+def match(text, trie):
     k = 0
-    symbol = list(text)[k]
     currentNode = 0
-    while True:
+    for i in range(len(text)):
+        symbol = list(text)[k]
         edges = list(trie[currentNode].keys())
-        if(edges == []):
-            return True
-        try:
-            symbol = list(text)[k]
-        except IndexError:
-            return False
+        if(symbol in edges):
+            k += 1
+            currentNode = trie[currentNode][symbol]
+            if (list(trie[currentNode].keys()) == []):
+                return True
         else:
-            if(symbol in edges):
-                currentNode = trie[currentNode][symbol]
-                k += 1
-            else:
-                return False
+            return False
+    return False
 
 def trie_pattern_match(text, trie):
     myAnswer = []
     for i in range(0, len(text)):
-        myText = text[i:len(text)]
-        if prefix_trie_match(myText, trie):
+        myText = text[i: (i + 100)]
+        if match(myText, trie):
             myAnswer.append(i)
     for j in myAnswer:
         print(j, end=' ')
-
-def trie_contruct(patterns):
-    trie = {0:{}}
-    root = 0
-    for pattern in patterns:
-        currentNode = root
-        for char in list(pattern):
-            part = trie[currentNode]
-            if(char in part.keys()):
-                currentNode = part[char]
-            else:
-                currentNode = len(trie)
-                trie[currentNode] = {}
-                part[char] = currentNode
-    return(trie)
-
-text = sys.stdin.readline().strip ()
-n = int(sys.stdin.readline().strip ())
-patterns = []
-for i in range(n):
-    patterns += [sys.stdin.readline().strip()]
-trie = trie_contruct(patterns)
-trie_pattern_match(text, trie)
